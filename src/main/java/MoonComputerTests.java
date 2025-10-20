b168ee8011a91885ce1bddf599772bba93299fa0
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-21 00:49:13 +0300

    Task completed 1

--- src/main/java/MoonComputerTests.java
+++ src/main/java/MoonComputerTests.java
@@ -4,10 +4,12 @@ public class MoonComputerTests {
 
     public void testBasicInputOutput() {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Enter your request: ");
+        String input = scanner.nextLine();
+        System.out.println(input);
+        scanner.close();
     }
 
-    //Test output on Hello Java standard input
     public static void main(String[] args) {
         MoonComputerTests computerTests = new MoonComputerTests();
         computerTests.testBasicInputOutput();
