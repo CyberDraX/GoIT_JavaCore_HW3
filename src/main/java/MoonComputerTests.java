b274a039ca0a258f70dc9543cb2f5b4a3e780444
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-21 00:25:39 +0300

    Add new Class

---
+++ src/main/java/MoonComputerTests.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MoonComputerTests {
+
+    public void testBasicInputOutput() {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+    //Test output on Hello Java standard input
+    public static void main(String[] args) {
+        MoonComputerTests computerTests = new MoonComputerTests();
+        computerTests.testBasicInputOutput();
+    }
+}
