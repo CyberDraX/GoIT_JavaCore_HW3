9a257b2a0494a820a6b3acc596811f402daea497
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-21 22:57:46 +0300

    Task complete 6

--- src/main/java/HarekCity.java
+++ src/main/java/HarekCity.java
@@ -6,8 +6,15 @@ public class HarekCity {
         return result;
     }
 
+    public int[] createAgeArray(int age1, int age2, int age3, int age4) {
+        return new int[]{age1, age2, age3, age4};
+    }
+
     public static void main(String[] args) {
         String[] arr = new HarekCity().createEmptyNameArray();
         System.out.println(Arrays.toString(arr));
+
+        int[] ageArray = new HarekCity().createAgeArray(10, 20, 30, 40);
+        System.out.println(Arrays.toString(ageArray));
     }
 }
