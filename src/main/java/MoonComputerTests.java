87c73a49d1d330cde4399d804ec220e92a756d3c
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-21 21:36:05 +0300

    Task completed 2

--- src/main/java/MoonComputerTests.java
+++ src/main/java/MoonComputerTests.java
@@ -8,10 +8,23 @@ public class MoonComputerTests {
         String input = scanner.nextLine();
         System.out.println(input);
         scanner.close();
+
+    }
+
+    public void testMath() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter your second number");
+        int num2 = scanner.nextInt();
+        scanner.close();
+        System.out.println((num1 + num2) + " " + (num1 - num2) + " " + (num1 * num2) + " " + (num1 / num2));
     }
 
     public static void main(String[] args) {
         MoonComputerTests computerTests = new MoonComputerTests();
+
         computerTests.testBasicInputOutput();
+        computerTests.testMath();
     }
 }
