6467607c7ea5641020099b5b0d32282847e9e7da
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-22 22:27:20 +0300

    Task complete 16

--- src/main/java/SaveStarShip.java
+++ src/main/java/SaveStarShip.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
 public class SaveStarShip {
 
     public int calculateDistance(int distance) {
         return distance > 0 ? distance : distance / -1;
+    }
+
+    public String[] getPlanets(String galaxy) {
+
+        String galaxyPlanets;
+        if (galaxy.equals("Miaru")) {
+            galaxyPlanets = "Maux, Reux, Piax";
+        } else if (galaxy.equals("Milkyway")) {
+            galaxyPlanets = "Earth, Mars, Jupiter";
+        } else if (galaxy.equals("DangerBanger")) {
+            galaxyPlanets = "Fobius, Demius";
+        } else {
+            galaxyPlanets = "";
+        }
+        return  new String[] {galaxyPlanets};
     }
 
     public static void main(String[] args) {
@@ -9,5 +26,8 @@ public class SaveStarShip {
 
         //Should be 10
         System.out.println(ship.calculateDistance(-10));
+
+        //Should be [Fobius, Demius]
+        System.out.println(Arrays.toString(ship.getPlanets("DangerBanger")));
     }
 }
